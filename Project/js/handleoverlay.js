a06b85db9b981d3a24f2fe72f8874258f87c1294
HoangLoc03 <hoangphuocloc.phurieng@gmail.com>
2023-04-22 19:57:54 +0700

    update #8

--- Project/js/handleoverlay.js
+++ Project/js/handleoverlay.js
@@ -1,57 +1,58 @@
-const menu = document.querySelector('.nav-icon-dash')
-const openOverlay = document.querySelector('.include-overlay')
-const closeOverlay = document.querySelector('.icon-close-overlay')
-const main = document.body
-const searchMobile = document.querySelector('.search-mobile')
-const cancel = document.querySelector('.cancel-search')
-const toggleTitle = document.querySelector('.hide-show-item-children')
-const showTitle = document.querySelector('.footer-item-children')
-const toggleTitle2 = document.querySelector('.hide-show-item-children-2')
-const showTitle2 = document.querySelector('.footer-item-children-2')
-const showMobile = document.querySelector('.search')
+const menu = document.querySelector('.nav-icon-dash');
+const openOverlay = document.querySelector('.include-overlay');
+const closeOverlay = document.querySelector('.icon-close-overlay');
+const main = document.body;
+const searchMobile = document.querySelector('.search-mobile');
+const cancel = document.querySelector('.cancel-search');
+const toggleTitle = document.querySelector('.hide-show-item-children');
+const showTitle = document.querySelector('.footer-item-children');
+const toggleTitle2 = document.querySelector('.hide-show-item-children-2');
+const showTitle2 = document.querySelector('.footer-item-children-2');
+const showMobile = document.querySelector('.search');
 
 menu.addEventListener('click', () => {
-    openOverlay.classList.toggle("nav-overlay-open")
-    main.style.overflow = "auto"
-    main.classList.toggle("pre-noscroll")
-})
+    openOverlay.classList.toggle('nav-overlay-open');
+    main.style.overflow = 'auto';
+    main.classList.toggle('pre-noscroll');
+});
 
 closeOverlay.addEventListener('click', () => {
     openOverlay.onclick = (e) => {
         e.stopPropagation();
+    };
+    openOverlay.classList.toggle('nav-overlay-open');
+    main.style.overflow = 'scroll';
+    main.classList.toggle('pre-noscroll');
+});
+
+toggleTitle.addEventListener('click', () => {
+    toggleTitle.classList.toggle('fa-plus');
+    toggleTitle.classList.toggle('icon-add-mobile');
+    toggleTitle.classList.toggle('fa-minus');
+    toggleTitle.classList.toggle('icon-minus-mobile');
+    showTitle.classList.toggle('show-item-children');
+});
+
+toggleTitle2.addEventListener('click', () => {
+    toggleTitle2.classList.toggle('fa-plus');
+    toggleTitle2.classList.toggle('icon-add-mobile');
+    toggleTitle2.classList.toggle('fa-minus');
+    toggleTitle2.classList.toggle('icon-minus-mobile');
+    showTitle2.classList.toggle('show-item-children-2');
+});
+
+const x = window.matchMedia('(max-width: 992px)');
+
+function myFunction(x) {
+    if (x.matches) {
+        showMobile.addEventListener('click', (e) => {
+            e.preventDefault();
+            searchMobile.style.display = 'inherit';
+        });
+        cancel.addEventListener('click', () => {
+            searchMobile.style.display = 'none';
+        });
     }
-    openOverlay.classList.toggle("nav-overlay-open")
-    main.style.overflow = "scroll"
-    main.classList.toggle("pre-noscroll")
-})    
+}
 
-
-toggleTitle.addEventListener("click", () => {
-    toggleTitle.classList.toggle("fa-plus");
-    toggleTitle.classList.toggle("icon-add-mobile");
-    toggleTitle.classList.toggle("fa-minus");
-    toggleTitle.classList.toggle("icon-minus-mobile");
-    showTitle.classList.toggle("show-item-children");
-})
-
-toggleTitle2.addEventListener("click", () => {
-    toggleTitle2.classList.toggle("fa-plus");
-    toggleTitle2.classList.toggle("icon-add-mobile");
-    toggleTitle2.classList.toggle("fa-minus");
-    toggleTitle2.classList.toggle("icon-minus-mobile");
-    showTitle2.classList.toggle("show-item-children-2");
-})
-
-
-            showMobile.addEventListener("click", (e) => { 
-                e.preventDefault();
-                searchMobile.style.display = "inherit";
-            });
-            cancel.addEventListener("click", () => {
-                searchMobile.style.display = "none"
-            })
-
-
-
-
-
+myFunction(x); // Call listener function at run time
--- Project/js/handleshow.js
+++ Project/js/handleshow.js
@@ -1,25 +1,21 @@
-const showUl1 = document.getElementById('ul1')
-const showUl2 = document.getElementById('ul2')
-const showUl3 = document.getElementById('ul3')
-const showUl4 = document.getElementById('ul4')
+const showUl1 = document.getElementById('ul1');
+const showUl2 = document.getElementById('ul2');
+const showUl3 = document.getElementById('ul3');
+const showUl4 = document.getElementById('ul4');
 const show = document.querySelector('#show1');
 const show2 = document.querySelector('#show2');
 const show3 = document.querySelector('#show3');
 const show4 = document.querySelector('#show4');
 
-
 showUl1.addEventListener('click', () => {
     show.classList.toggle('showtag');
-})
+});
 showUl2.addEventListener('click', () => {
     show2.classList.toggle('showtag');
-})
+});
 showUl3.addEventListener('click', () => {
     show3.classList.toggle('showtag');
-})
+});
 showUl4.addEventListener('click', () => {
     show4.classList.toggle('showtag');
-})
-
-var scrollSpyContentEl = document.querySelector()
-var scrollSpy = bootstrap.ScrollSpy.getOrCreateInstance(scrollSpyContentEl) // Returns a Bootstrap scrollspy instance
+});
