fdb77957f6aea12b2fd966e24710c2c510258f3e
HoangLoc03 <hoangphuocloc.phurieng@gmail.com>
2023-05-11 22:49:08 +0700

    new update

--- Project/js/errorform.js
+++ Project/js/errorform.js
@@ -1,87 +1,93 @@
 // hàm error
 function ErrorMessage(options) {
-
     //hàm thực hiện validation
     function handleValidate(inputElement, rule) {
-
-        var parentErrorElement = inputElement.parentElement.querySelector(options.errorNotify);
-
+        var parentErrorElement = inputElement.parentElement.querySelector(
+            options.errorNotify,
+        );
 
         var errorMessageNotify = rule.test(inputElement.value);
-                    
-                    if (errorMessageNotify) {
-                        parentErrorElement.innerText = errorMessageNotify;
-                        inputElement.parentElement.classList.add('invalid-error');
-                    }
-                    else {
-                    parentErrorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid-error');
-                    }
+
+        if (errorMessageNotify) {
+            parentErrorElement.innerText = errorMessageNotify;
+            inputElement.parentElement.classList.add('invalid-error');
+        } else {
+            parentErrorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid-error');
+        }
     }
 
     //lấy element form cần thực hiện validation
     var formElement = document.querySelector(options.form);
 
-
-    if(formElement) {
+    if (formElement) {
         options.rules.forEach(function (rule) {
-            
             var inputElement = formElement.querySelector(rule.selector);
 
-            if(inputElement) {
-                inputElement.onblur = function() {
-                    handleValidate(inputElement,rule);
-            }
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    handleValidate(inputElement, rule);
+                };
 
-            // xử lí khi người dùng đang nhập 
-            inputElement.oninput = function() {
-            var parentErrorElement = inputElement.parentElement.querySelector(options.errorNotify);
+                // xử lí khi người dùng đang nhập
+                inputElement.oninput = function () {
+                    var parentErrorElement =
+                        inputElement.parentElement.querySelector(
+                            options.errorNotify,
+                        );
 
-                parentErrorElement.innerText = '';
-                inputElement.parentElement.classList.remove('invalid-error');
+                    parentErrorElement.innerText = '';
+                    inputElement.parentElement.classList.remove(
+                        'invalid-error',
+                    );
+                };
             }
-         }
         });
     }
 }
- 
 
 //rules
 
-ErrorMessage.isEmail = function(selector) {
+ErrorMessage.isEmail = function (selector) {
     return {
         selector: selector,
-        test: function(value) {
+        test: function (value) {
             var checkRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return checkRegex.test(value) ? undefined : 'Vui lòng nhập đúng email';
-        }
-    }
-}
+            return checkRegex.test(value)
+                ? undefined
+                : 'Vui lòng nhập đúng email';
+        },
+    };
+};
 
-ErrorMessage.isPassword = function(selector,lenght) {
+ErrorMessage.isPassword = function (selector) {
     return {
         selector: selector,
-        lenght: lenght,
-        test: function(value) {
-            return value.trim() && value.lenght >= lenght ? undefined : 'Mật khẩu tối thiểu 6 kí tự'
-        }
-    }
-}
+        test: function (value) {
+            var checkRegexpw =
+                /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/;
 
-ErrorMessage.isFirstName = function(selector) {
+            return checkRegexpw.test(value)
+                ? undefined
+                : 'Mật khẩu tối thiểu 6 kí tự, bao gồm 1 kí tự hoa, 1 kí tự thường, 1 kí tự số và 1 kí tự đặc biệt';
+        },
+    };
+};
+
+ErrorMessage.isFirstName = function (selector) {
     return {
         selector: selector,
-        test: function(value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
-        }
-    }
-}
+        test: function (value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này';
+        },
+    };
+};
 
-ErrorMessage.isLastName = function(selector) {
+ErrorMessage.isLastName = function (selector) {
     return {
         selector: selector,
-        test: function(value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
-        }
-    }
-}
+        test: function (value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này';
+        },
+    };
+};
